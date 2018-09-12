48947025489829331ab22ba58e56e3a530e20841
Nathan Ostrander <14nostrander@gmail.com>
2018-09-11 20:57:34 -0400

    add dependencies

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  'app.component.ts'
+  @ViewChild('snav') public snav;
   constructor() { }
 
   ngOnInit() {
